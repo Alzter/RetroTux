e0f2eb8c32f930bdb46931f6d5fc363b1399d820
Alzter <alexsmudgy20@gmail.com>
2020-05-10 18:30:36 +1000

    that's progress enough for today
    todo: i hate screwing around with state machines

--- src/Scenes/Players/Player.gd
+++ src/Scenes/Players/Player.gd
@@ -1,3 +1,11 @@
 extends KinematicBody2D
 
+var FLOOR = Vector2(0, -1)
 var velocity = Vector2()
+
+func _physics_process(delta):
+	move_and_slide(velocity, FLOOR)
+	
+
+func get_input():
+	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
