1e0f2d99b46557c32089892c0408020d9877c714
Alzter <alexsmudgy20@gmail.com>
2020-05-10 17:30:16 +1000

    Initial commit - basic test level and background

---
+++ src/Scenes/Players/SMPlayer.gd
@@ -0,0 +1,38 @@
+extends "res://Scenes/Master/StateMachine.gd"
+
+func _ready():
+	add_state("idle")
+	add_state("run")
+	add_state("jump")
+	call_deferred("set_state", states.idle)
+
+func _state_logic(delta):
+	pass
+
+func _get_transition(delta):
+	match state:
+		states.idle:
+			if !parent.is_on_floor():
+				return states.jump
+			elif parent.velocity.x != 0:
+				return states.run
+		states.run:
+			if !parent.is_on_floor():
+				return states.jump
+			elif parent.velocity.x == 0:
+				return states.idle
+		states.jump:
+			if parent.is_on_floor():
+				return states.idle
+
+func _enter_state(new_state, old_state):
+	match new_state:
+		states.idle:
+			pass
+		states.run:
+			pass
+		states.jump:
+			pass
+
+func _exit_state(old_state, new_state):
+	pass
