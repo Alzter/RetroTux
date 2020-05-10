f5ad687d597185ea04950d8200b2c0e05b590bb8
Alzter <alexsmudgy20@gmail.com>
2020-05-10 20:24:42 +1000

    slope physics: this'll be a doozy

--- src/Scenes/Players/Player.gd
+++ src/Scenes/Players/Player.gd
@@ -1,11 +1,19 @@
 extends KinematicBody2D
 
-var FLOOR = Vector2(0, -1)
 var velocity = Vector2()
 
-func _physics_process(delta):
-	move_and_slide(velocity, FLOOR)
-	
+const MOVE_SPEED = 40
+const FRICTION = 0.8
+const GRAVITY = 20
 
-func get_input():
+func _physics_process(delta):
+	move_inputs()
+	velocity.y += GRAVITY * delta * 60
+	velocity = move_and_slide_with_snap(velocity, Vector2(0, -1), Vector2(0, 32))
+
+func move_inputs():
+	velocity.x *= FRICTION
 	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
+	velocity.x += MOVE_SPEED * move_direction
+	if abs(velocity.x) < 6:
+		velocity.x = 0
